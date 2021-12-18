193e58076b8085f9006ca862b2f7f83657fc3915
Makarov Andrey <winandfx@gmail.com>
2021-12-18 13:38:40 +0300

    Fix: red color range is divided into 2 ranges
    See also https://stackoverflow.com/q/30331944

--- test.py
+++ test.py
@@ -12,19 +12,20 @@ while True:
     _, frame = cap.read()
     # Convert BGR to HSV
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-    # Threshold the HSV image to get only blue colors
-    lower_color = [color_slider.hue] + list(color_slider.lower_color)
-    lower_color[0] = lower_color[0] - hue_width
-    if lower_color[0] < 0:
-        lower_color[0] = 180 + lower_color[0]
-    upper_color = [color_slider.hue] + list(color_slider.upper_color)
-    upper_color[0] = upper_color[0] + hue_width
-    if upper_color[0] > 179:
-        upper_color[0] = 180 - upper_color[0]
-    # print(lower_color, upper_color)
-    mask = cv.inRange(hsv, np.uint8(lower_color), np.uint8(upper_color))
+    # Threshold the HSV image to get only colors in selected range
+    lower_color = list(color_slider.lower_color)
+    lower_color[0] = color_slider.shift_hue(-hue_width)
+    upper_color = list(color_slider.upper_color)
+    upper_color[0] = color_slider.shift_hue(+hue_width)
+    if lower_color[0] > upper_color[0]:
+        # https://stackoverflow.com/q/30331944
+        mask1 = cv.inRange(hsv, np.uint8(lower_color), np.uint8([179, *upper_color[1:]]))
+        mask2 = cv.inRange(hsv, np.uint8([0, *lower_color[1:]]), np.uint8(upper_color))
+        mask = mask1 + mask2
+    else:
+        mask = cv.inRange(hsv, np.uint8(lower_color), np.uint8(upper_color))
     # Bitwise-AND mask and original image
-    res = cv.bitwise_and(frame,frame, mask= mask)
+    res = cv.bitwise_and(frame, frame, mask=mask)
     cv.imshow('frame',frame)
     cv.imshow('mask',mask)
     cv.imshow('res',res)
